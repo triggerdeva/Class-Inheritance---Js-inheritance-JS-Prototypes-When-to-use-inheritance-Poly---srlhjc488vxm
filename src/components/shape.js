d45537f7dd7ab9b895f8ae28a1672484d8cd8448
triggerdeva <120394539+triggerdeva@users.noreply.github.com>
2023-01-23 23:54:03 +0530

    update

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,21 @@
+// import using require
+
 // declare class
 
-// export class using module.exports
+
+// export class using module.exportsconst Shape = require('./shape');
+const Shape = require('./shape');
+
+class Circle extends Shape {
+    constructor(color) {
+        super(color);
+    }
+
+    calculateArea() {
+        console.log("Calculating area of Circle");
+    }
+}
+
+module.exports = Circle;
+
+
